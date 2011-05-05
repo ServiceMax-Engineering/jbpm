4212664354c7a0c0f92e08d46574b2755cdc49af
melendeznicolas <melendez@intalio.com>
2011-05-05 17:20:41 -0300

    catch and link node instance skeleton

---
+++ jbpm-flow/src/main/java/org/jbpm/workflow/instance/node/CatchLinkNodeInstance.java
@@ -0,0 +1,15 @@
+package org.jbpm.workflow.instance.node;
+
+import org.drools.runtime.process.NodeInstance;
+import org.jbpm.workflow.instance.impl.NodeInstanceImpl;
+
+public class CatchLinkNodeInstance extends NodeInstanceImpl {
+
+	private static final long serialVersionUID = 20110505L;
+
+	@Override
+	public void internalTrigger(NodeInstance from, String type) {
+
+	}
+
+}
