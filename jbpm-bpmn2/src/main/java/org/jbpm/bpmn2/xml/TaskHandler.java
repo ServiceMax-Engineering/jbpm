3af0205643a577c852227713d107f2ead8d8a2fa
feller <feller@intalio.com>
2011-05-12 13:02:18 -0300

    removed test method

--- jbpm-bpmn2/src/main/java/org/jbpm/bpmn2/xml/TaskHandler.java
+++ jbpm-bpmn2/src/main/java/org/jbpm/bpmn2/xml/TaskHandler.java
@@ -61,10 +61,6 @@ public class TaskHandler extends AbstractNodeHandler {
 	private Map<String, String> dataInputs = new HashMap<String, String>();
 	private Map<String, String> dataOutputs = new HashMap<String, String>();
 
-	protected void testMethod(){
-		System.out.println("===FEDE===");
-	}
-
     protected Node createNode(Attributes attrs) {
         return new WorkItemNode();
     }
